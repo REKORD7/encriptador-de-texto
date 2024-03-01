8b0a31062f519a5a311b80974fa5514dc666dc21
Manuel.Gomez <godm8905@gmail.com>
2024-02-29 17:11:57 -0700

    Update app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const textoInsertado = document.getElementById("texto-insertado");
+const mensajeEncriptado = document.getElementById("texto-a-copiar");
+
+const llavesDeEncriptacion = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
